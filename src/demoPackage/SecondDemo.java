292d6015ebee4014599732ef81c869fcedeaa86e
palla <palla@LAPTOP-T4FTLVMF>
2022-06-28 23:18:19 +0530

    Second and Third commit

---
+++ src/demoPackage/SecondDemo.java
@@ -0,0 +1,12 @@
+package demoPackage;
+
+public class SecondDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+
+	}
+
+}
---
+++ src/demoPackage/ThirdDemo.java
@@ -0,0 +1,12 @@
+package demoPackage;
+
+public class ThirdDemo
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome");
+
+	}
+
+}
